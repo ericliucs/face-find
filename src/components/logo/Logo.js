d42e426c70652506e57934c23a26f550371698b6
Eric Liu <liue@duq.edu>
2024-04-12 07:26:08 -0400

    Created logo and sign out button components

---
+++ src/components/logo/Logo.js
@@ -0,0 +1,11 @@
+import Tilt from "react-parallax-tilt";
+
+function Logo() {
+  return <div className={"m-8"}>
+    <Tilt className={"w-64"}>
+      <h1 className={"text-4xl"}>face-find 👀</h1>
+    </Tilt>
+  </div>;
+}
+
+export default Logo;
---
+++ src/components/navigation/Navigation.js
@@ -0,0 +1,8 @@
+function Navigation() {
+  return <nav className={"flex justify-end"}>
+    <button className={"px-4 py-2 rounded-lg bg-slate-900 align-middle text-xl transition duration-100" +
+      " hover:bg-slate-800"}>Sign Out</button>
+  </nav>;
+}
+
+export default Navigation;
