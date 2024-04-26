fbef346f4dd39acc7dca68efb6279a500126bc21
Eric Liu <liue@duq.edu>
2024-04-26 01:16:40 -0400

    Added tsparticles

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 ### Overview
 A full-stack web app that detects faces in submitted images.
+
+Uses [@tsparticles/react](https://www.npmjs.com/package/@tsparticles/react) for particle effects.
--- src/App.js
+++ src/App.js
@@ -2,9 +2,100 @@ import ImageLinkForm from "./components/logo/ImageLinkForm";
 import Logo from "./components/logo/Logo";
 import Navigation from "./components/navigation/Navigation";
 import Rank from "./components/rank/Rank";
+import { useEffect, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadSlim } from "@tsparticles/slim";
+
+const particlesOptions = {
+  background: {
+    color: {
+      value: "#000000",
+    },
+  },
+  fpsLimit: 144,
+  interactivity: {
+    events: {
+      onClick: {
+        enable: false,
+        mode: "push",
+      },
+      onHover: {
+        enable: true,
+        mode: "repulse",
+      },
+      resize: true,
+    },
+    modes: {
+      push: {
+        quantity: 4,
+      },
+      repulse: {
+        distance: 120,
+        duration: 0.4,
+      },
+    },
+  },
+  particles: {
+    color: {
+      value: "#ffffff",
+    },
+    links: {
+      color: "#ffffff",
+      distance: 150,
+      enable: true,
+      opacity: 0.5,
+      width: 1,
+    },
+    move: {
+      direction: "none",
+      enable: true,
+      outModes: {
+        default: "bounce",
+      },
+      random: false,
+      speed: 3,
+      straight: false,
+    },
+    number: {
+      density: {
+        enable: true,
+        area: 800,
+      },
+      value: 80,
+    },
+    opacity: {
+      value: 0.7,
+    },
+    shape: {
+      type: "circle",
+    },
+    size: {
+      value: { min: 1, max: 5 },
+    },
+  },
+  detectRetina: true,
+}
 
 function App() {
+  const [init, setInit] = useState(false);
+
+  // this should be run only once per application lifetime
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadSlim(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
+
   return <div id={"app"} className={"app"}>
+    {init &&
+      <Particles id="tsparticles" particlesLoaded={particlesLoaded} options={particlesOptions}
+                 className={"fixed -z-50"}/>}
     <div id={"navbar"} className={"flex justify-between text-center"}>
       <Logo />
       <Navigation />
@@ -14,6 +105,8 @@ function App() {
       <ImageLinkForm />
     </div>
   </div>;
+
+
 }
 
 export default App;
